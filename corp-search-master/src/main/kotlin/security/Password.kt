e079768cbbc081b7eceec69c9bb855ebe6afa94b
CatatonicChimp1 <114211884+CatatonicChimp1@users.noreply.github.com>
2026-04-01 17:08:19 +0300

    Delete corp-search-master directory

--- corp-search-master/src/main/kotlin/security/Password.kt
+++
@@ -1,8 +0,0 @@
-package com.example.security
-
-import org.mindrot.jbcrypt.BCrypt
-
-object Passwords {
-    fun hash(password: String): String = BCrypt.hashpw(password, BCrypt.gensalt(12))
-    fun verify(password: String, hash: String): Boolean = BCrypt.checkpw(password, hash)
-}
